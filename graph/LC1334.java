b8a360509f84aa01232023213093ffc2c79176d1
gir1yaphets <763791240@qq.com>
2020-02-16 12:12:27 -0800

    Update question

--- graph/LC1334.java
+++ graph/LC1334.java
@@ -1,5 +1,5 @@
 class LC1334 {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+    public int findTheCity_Floyed(int n, int[][] edges, int distanceThreshold) {
         int[][] dp = new int[n][n];
         
         //如果两个点非连通，为了后面能将该两点间距离更新为经过某个中间点的距离，所以这里要初始化为最大值
@@ -43,4 +43,79 @@ class LC1334 {
         return res;
     }
     
+    private Map<Integer, List<int[]>> g = new HashMap<>();
+    
+    public int findTheCity_dij(int n, int[][] edges, int distanceThreshold) {
+        buildGraph(edges);
+        
+        int res = 0;
+        int min = n;
+        
+        for (int i = 0; i < n; i++) {
+            int[] costs = dij(n, i);
+            
+            int count = 0;
+            
+            for (int cost : costs) {
+                if (cost <= distanceThreshold) {
+                    count += 1;
+                }
+            }
+        
+            if (count <= min) {
+                min = count;
+                res = i;
+            }
+        }
+        
+        return res;
+    }
+    
+    private void buildGraph(int[][] edges) {
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            
+            List<int[]> ulist = g.getOrDefault(u, new ArrayList<>());
+            List<int[]> vlist = g.getOrDefault(v, new ArrayList<>());
+            
+            ulist.add(new int[]{v, w});
+            vlist.add(new int[]{u, w});
+            
+            g.put(u, ulist);
+            g.put(v, vlist);
+        }
+    }
+    
+    private int[] dij(int n, int src) {
+        int[] cost = new int[n];
+        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> cost[o1] - cost[o2]);
+        
+        Arrays.fill(cost, 10001);
+        cost[src] = 0;
+        pq.offer(src);
+        
+        while (!pq.isEmpty()) {
+            int v = pq.poll();
+            visited.add(v);
+            
+            List<int[]> nbList = g.get(v);
+            if (nbList != null) {
+                for (int[] nb : nbList) {
+                    int u = nb[0], w = nb[1];
+                    
+                    if (visited.contains(u)) {
+                        continue;
+                    }
+                    
+                    if (cost[u] > w + cost[v]) {
+                        cost[u] = w + cost[v];
+                        pq.offer(u);
+                    }
+                }
+            }
+        }
+        
+        return cost;
+    }
 }
